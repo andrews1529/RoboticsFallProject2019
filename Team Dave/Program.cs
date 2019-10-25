adea615d04d3876e0bd383b1dcfa1064ee63550e
andrews1529 <56899838+andrews1529@users.noreply.github.com>
2019-10-24 17:33:26 -0700

    Add code for turning and second talon

--- Team Dave/Program.cs
+++ Team Dave/Program.cs
@@ -14,22 +14,31 @@ namespace Team_Dave
 
             /* create a talon, the Talon Device ID in HERO LifeBoat is zero */
             CTRE.Phoenix.MotorControl.CAN.TalonSRX myTalon = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(1);
+            CTRE.Phoenix.MotorControl.CAN.TalonSRX myTalon2 = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(2);
 
-            float speed = myGamepad.GetAxis(1);
+            float speed = myGamepad.GetAxis(1) * 1;
+            float turn = myGamepad.GetAxis(0) * 1;
 
             /* loop forever */
             while (true)
             {
                 speed = myGamepad.GetAxis(1);
+                turn = myGamepad.GetAxis(0) * 1;
 
                 /* added inside the while loop */
                 if (myGamepad.GetConnectionStatus() == CTRE.Phoenix.UsbDeviceConnection.Connected)
                 {
                     /* print the axis value */
                     Debug.Print("axis:" + myGamepad.GetAxis(0) + ", " + myGamepad.GetAxis(1) + ", " + myGamepad.GetAxis(2) + ", " + myGamepad.GetAxis(5));
+                    Debug.Print("speed:" + speed);
+                    Debug.Print("turn:" + turn);
 
                     /* pass axis value to talon */
                     myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, speed);
+                    myTalon2.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, speed);
+
+                    myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, turn);
+                    myTalon2.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, turn * -1);
 
                     /* allow motor control */
                     CTRE.Phoenix.Watchdog.Feed();
