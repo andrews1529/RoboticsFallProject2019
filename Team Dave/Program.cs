d69017d406d0944913fa30db99895704ab717113
andrews1529 <56899838+andrews1529@users.noreply.github.com>
2019-11-02 10:00:21 -0700

    Add turning

--- Team Dave/Program.cs
+++ Team Dave/Program.cs
@@ -16,29 +16,38 @@ namespace Team_Dave
             CTRE.Phoenix.MotorControl.CAN.TalonSRX myTalon = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(1);
             CTRE.Phoenix.MotorControl.CAN.TalonSRX myTalon2 = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(2);
 
-            float speed = myGamepad.GetAxis(1) * 1;
-            float turn = myGamepad.GetAxis(0) * 1;
+            float speed;
+            float turn;
+            float avg;
 
             /* loop forever */
             while (true)
             {
-                speed = myGamepad.GetAxis(1);
-                turn = myGamepad.GetAxis(0) * 1;
 
                 /* added inside the while loop */
                 if (myGamepad.GetConnectionStatus() == CTRE.Phoenix.UsbDeviceConnection.Connected)
                 {
+                    //If you want to change it to right bumper use get.axis(2) and (5)
+                    speed = myGamepad.GetAxis(1);
+                    turn = myGamepad.GetAxis(0);
+                    avg = speed / 2 + turn / 2;
                     /* print the axis value */
+                    //Turning is overriding the speed 
                     Debug.Print("axis:" + myGamepad.GetAxis(0) + ", " + myGamepad.GetAxis(1) + ", " + myGamepad.GetAxis(2) + ", " + myGamepad.GetAxis(5));
                     Debug.Print("speed:" + speed);
                     Debug.Print("turn:" + turn);
 
                     /* pass axis value to talon */
-                    myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, speed);
-                    myTalon2.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, speed);
+                   // myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, speed);
+                   // myTalon2.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, speed);
 
-                    myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, turn);
-                    myTalon2.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, turn * -1);
+                    //myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, turn);
+                    //myTalon2.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, turn * -1);
+
+                    
+                    myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, avg * -1);
+                    myTalon2.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, avg);
+
 
                     /* allow motor control */
                     CTRE.Phoenix.Watchdog.Feed();
